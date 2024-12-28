80833089dc6c53d671b87ec7c3ae5413b06fdb80
Avinash <khadsanavinash01@gmail.com>
2024-12-28 08:35:27 +0530

    WALIA-07 Integrate thymeleaf with spring boot

--- src/main/java/com/walia/controller/UserController.java
+++ src/main/java/com/walia/controller/UserController.java
@@ -2,24 +2,32 @@ package com.walia.controller;
 
 import com.walia.dto.UserDto;
 import com.walia.service.UserService;
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @PostMapping(path="/create", produces= MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path = "/create", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UserDto> create(@RequestBody UserDto req) {
-        System.out.println("Create user initiated: "+req.getFirstName());
+        System.out.println("Create user initiated: " + req.getFirstName());
         userService.create(req);
-        return  new ResponseEntity<UserDto>(req,HttpStatus.CREATED);
+        return new ResponseEntity<UserDto>(req, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/create")
+    public String create() {
+        return "default";
     }
 }
